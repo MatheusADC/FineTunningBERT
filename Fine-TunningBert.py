02b95ccf63e234b6ba67bf4e1488772d9d572a65
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-18 11:54:17 -0300

    add: Carregando o dataset

--- Fine-TunningBert.py
+++ Fine-TunningBert.py
@@ -4,3 +4,6 @@ from transformers import AutoTokenizer, DataCollatorWithPadding
 import os
 
 notebook_login()
+dataset = load_dataset("json", data_files={"train": "/content/train.jsonl", "test": "/content/validation.jsonl"})
+
+print(dataset)
