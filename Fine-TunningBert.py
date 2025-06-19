c9a66b09749f0af2ea3aa45755a8a82f44cf7c44
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-19 10:39:07 -0300

    add: Preparação do dataset

--- Fine-TunningBert.py
+++ Fine-TunningBert.py
@@ -7,3 +7,25 @@ notebook_login()
 dataset = load_dataset("json", data_files={"train": "/content/train.jsonl", "test": "/content/validation.jsonl"})
 
 print(dataset)
+
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+mapDict = {
+    "No Hate Speech": 0,
+    "Hate Speech": 1
+}
+
+def transform_labels(labels):
+  label = label['output']
+  result = []
+  for l in label:
+    result.append(mapDict[l])
+  return {"label": result}
+
+def tokenize_function(example):
+  return tokenizer(example['input'], padding=True, truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+tokenized_datasets = tokenized_datasets.map(transform_labels, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
